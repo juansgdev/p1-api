0d6158df3a7356fd0a9193c16505e38abbf04e02
juansgdev <juangomes.profissional@gmail.com>
2025-04-13 22:15:14 -0300

    Feat: user route added

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import http from 'http';
 import * as utils from './src/modules/utils/utils.js';
+import {log} from 'console';
 
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type':'text/json'});
-    const json = utils.readJSON('./src/assets/mock/mock.json');
-    res.write(json.username);
+
+    // ROTAS
+    if (req.url == '/user' && req.method == 'GET') {
+        res.write(utils.readFile('./cfg.json'));
+    }
+
+
+
+
+
+
+
     res.end();
 }).listen(2000);
